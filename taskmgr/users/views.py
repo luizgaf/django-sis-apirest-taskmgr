7a26f4b126e91a4447e578a7a889f1fe910cbac7
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-12-10 12:54:32 -0300

    first - auth system complete

---
+++ taskmgr/users/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate ,login as djangologin
+from django.contrib.auth.decorators import login_required
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            djangologin(request, user)
+            return plataforma(request)
+        else:
+            return HttpResponse('Nao foi possivel')
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'register.html')
+    
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        # validation:
+        usernameExists = User.objects.filter(username=username).first()
+        emailExisists = User.objects.filter(email=email).first()
+
+        if usernameExists:
+            return HttpResponse('Usuario existe')
+        
+        if emailExisists:
+            return HttpResponse('Email ja cadastrado')
+        
+        else:
+            user = User.objects.create_user(username, email, password)
+
+
+        return HttpResponse(username)
+
+@login_required(login_url="/auth/login/")
+def plataforma(request):   
+        return HttpResponse("plataforma")
